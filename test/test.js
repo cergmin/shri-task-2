51436e59219d210f4501416041dec8171cc5dc31
cergmin <cergmin@gmail.com>
2021-03-23 19:11:36 +1000

    Расширены тесты, добавлена проверка пустых спринтов и отсутсвия пользователей с нулевым valueText в массивх

--- test/test.js
+++ test/test.js
@@ -39,14 +39,35 @@ describe('Module check', function () {
                 }
             );
 
-            expect(sprintResultsSelection[sprintId]).to.be.an.instanceof(Array);
+            expect(
+                sprintResultsSelection[sprintId]
+            ).to.be.an.instanceof(Array);
         });
     }
 
     it('prepareData should return exactly 5 cards for any sprint', function () {
         for (let sprintId of sprintIdsSelection) {
-            expect(sprintResultsSelection[sprintId], 'Sprint #' + sprintId).to.have.lengthOf(5);
+            expect(
+                sprintResultsSelection[sprintId],
+                'prepareData should return 5 cards for sprint #'
+            ).to.have.lengthOf(5);
         }
+    });
+
+    it('prepareData should always return same cards for same input data', function () {
+        let cards1 = sprintResultsSelection[976];
+        let cards2 = prepareDataModule.prepareData(inputData, { sprintId: 976 });
+        let cards3 = prepareDataModule.prepareData(inputData, { sprintId: 976 });
+
+        expect(
+            cards1,
+            'Compare the 1st and 2nd calls of prepareData for sprint #976'
+        ).to.deep.equal(cards2);
+
+        expect(
+            cards1,
+            'Compare the 1st and 3rd calls of prepareData for sprint #976'
+        ).to.deep.equal(cards3);
     });
 });
 
@@ -67,17 +88,97 @@ describe('Checking result for the sprint #977', function () {
     }
 });
 
-describe('Checking result for the sprint #976', function () {
-    let data = prepareDataModule.prepareData(inputData, { sprintId: 976 });
+describe('Checking result for the sprint #981', function () {
+    let data = prepareDataModule.prepareData(inputData, { sprintId: 981 });
 
     it('differenceText in diagram card should have \'-\' before negative values', function () {
-        expect(data[3].data.categories[0].differenceText[0], 'Check for \'-\' in data.categories[0].differenceText[0]').to.equal('-');
+        expect(
+            data[3].data.categories[2].differenceText[0],
+            'Check for \'-\' in data.categories[2].differenceText[0]'
+        ).to.equal('-');
     });
 
     it('differenceText in diagram card should have \'+\' before positive values', function () {
-        expect(data[3].data.differenceText[0], 'data.differenceText[0]').to.equal('+');
-        expect(data[3].data.categories[1].differenceText[0], 'Check for \'+\' in data.categories[1].differenceText[0]').to.equal('+');
-        expect(data[3].data.categories[2].differenceText[0], 'Check for \'+\' in data.categories[2].differenceText[0]').to.equal('+');
-        expect(data[3].data.categories[3].differenceText[0], 'Check for \'+\' in data.categories[3].differenceText[0]').to.equal('+');
+        expect(
+            data[3].data.differenceText[0],
+            'data.differenceText[0]'
+        ).to.equal('+');
+
+        expect(
+            data[3].data.categories[1].differenceText[0],
+            'Check for \'+\' in data.categories[1].differenceText[0]'
+        ).to.equal('+');
+
+        expect(
+            data[3].data.categories[3].differenceText[0],
+            'Check for \'+\' in data.categories[3].differenceText[0]'
+        ).to.equal('+');
+    });
+
+    it('differenceText in diagram card should have no sign before zero values', function () {
+        expect(
+            data[3].data.categories[0].differenceText[0],
+            'Check for \'0\' in data.categories[0].differenceText[0]'
+        ).to.equal('0');
+    });
+});
+
+describe('Checking result for the sprint #990', function () {
+    let data = prepareDataModule.prepareData(inputData, { sprintId: 990 });
+
+    it('All users in the leaders card should have at least 1 commit', function () {
+        for (let i = 0; i < data[0].data.users.length; i++) {
+            expect(
+                Number(data[0].data.users[i].valueText)
+            ).to.be.at.least(1);
+        }
+    });
+
+    it('All users in the chart card should have at least 1 commit', function () {
+        for (let i = 0; i < data[2].data.users.length; i++) {
+            expect(
+                Number(data[2].data.users[i].valueText)
+            ).to.be.at.least(1);
+        }
+    });
+});
+
+describe('Checking result for the sprint #996 (empty sprint)', function () {
+    let data = prepareDataModule.prepareData(inputData, { sprintId: 996 });
+
+    it('Cards should have subtitle with sprint name', function () {
+        for (let i = 0; i < 5; i++) {
+            expect(
+                data[i].data.subtitle,
+                'Check subtitle in card №' + (i + 1)
+            ).to.equal('Бармалей');
+        }
+    });
+
+    it('Users array should be empty in the leaders card', function () {
+        // eslint-disable-next-line no-unused-expressions
+        expect(data[0].data.users).to.be.empty;
+    });
+
+    it('Users array should be empty in the vote card', function () {
+        // eslint-disable-next-line no-unused-expressions
+        expect(data[1].data.users).to.be.empty;
+    });
+
+    it('Users array should be empty in the chart card', function () {
+        // eslint-disable-next-line no-unused-expressions
+        expect(data[2].data.users).to.be.empty;
+    });
+
+    it('Activity card should have zero activity', function () {
+        const weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+        for (const weekday of weekdays) {
+            expect(data[4].data.data[weekday]).to.be.deep.equal([
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0
+            ]);
+        }
     });
 });
